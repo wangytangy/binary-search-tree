5786b3eda0ffe4a9e14c34a0775d51d64cf11b55
wangytangy <yue.bruce.wang@gmail.com>
2017-01-26 14:40:39 -0500

    Initial commit

---
+++ lib/bst.rb
@@ -0,0 +1,93 @@
+class BSTNode
+  attr_accessor :left, :right
+  attr_reader :value
+
+  def initialize(value)
+    @value = value
+    @left = nil
+    @right = nil
+  end
+end
+
+class BinarySearchTree
+  def initialize
+
+  end
+
+  def insert(value)
+
+  end
+
+  def find(value)
+
+  end
+
+  def inorder
+
+  end
+
+  def postorder
+
+  end
+
+  def preorder
+
+  end
+
+  def height
+
+  end
+
+  def min
+
+  end
+
+  def max
+
+  end
+
+  def delete(value)
+
+  end
+
+  def self.insert!(node, value)
+    node = BSTNode.new(value)
+    
+  end
+
+  def self.find!(node, value)
+
+  end
+
+  def self.preorder!(node)
+
+  end
+
+  def self.inorder!(node)
+
+  end
+
+  def self.postorder!(node)
+
+  end
+
+  def self.height!(node)
+
+  end
+
+  def self.max(node)
+
+  end
+
+  def self.min(node)
+
+  end
+
+  def self.delete_min!(node)
+
+  end
+
+  def self.delete!(node, value)
+
+  end
+end
